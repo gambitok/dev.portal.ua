d1459865673e72bed9a2795d1780f8239fdd356b
gambitok <gambitokgd@gmail.com>
2023-03-25 18:57:06 +0200

    feat: changed passwords

--- lib/mysql_class.php
+++ lib/mysql_class.php
@@ -18,7 +18,7 @@ class db {
         $this->host = 'localhost';
         $this->dbname = 'myparts_dba';
         $this->username = 'toko_usr';
-        $this->password = 'T0k0U&erPa$&w0rd#183729z';
+        $this->password = 'T0k0U$er183729Pa$$#';
     }
 
     public function connect()
@@ -88,7 +88,7 @@ class dbt {
         $this->host = 'localhost';
         $this->dbname = 'toko_dba';
         $this->username = 'toko_usr';
-        $this->password = 'T0k0U&erPa$&w0rd#183729z';
+        $this->password = 'T0k0U$er183729Pa$$#';
     }
 
     public function connect()
@@ -156,10 +156,10 @@ class dbc
 
     private function load_auth_param()
     {
-        $this->host = '172.17.0.1';
+        $this->host = 'localhost';
         $this->dbname = 'toko_dba_cache';
         $this->username = 'toko_usr';
-        $this->password = 'T0k0U&erPa$&w0rd#183729z';
+        $this->password = 'T0k0U$er183729Pa$$#';
     }
 
     public function connect()
